9f9e7bc829df806c873b1dd0e1f865a28f648a69
zhouzz <2248869976@qq.com>
2022-12-05 17:50:51 +0800

    第一次提交gittestone

---
+++ gittestone/src/test/java/com/zhouzz/GittestoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhouzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittestoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

918567ec96959eee72e862cc628c1c8e94b18063
zhouzz <2248869976@qq.com>
2022-12-06 16:45:13 +0800

    第二次提交代码到本地仓库中

---
+++ gittestone/src/main/java/com/zhouzz/test/TestDemo.java
@@ -0,0 +1,16 @@
+package com.zhouzz.test;
+
+/**
+ * @author zhouzz
+ * @version 1.0
+ * @date 2022/12/6
+ * @time 16:42
+ * @desc
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+
+
+        System.out.println("擦爱吃123545U");
+    }
+}
